28d313d232e97f8a899a25aef00a6dd95499d86b
dharmishtha <114208599+dharmishthamakwana@users.noreply.github.com>
2022-12-15 18:02:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,204 +1,378 @@
+// import 'package:flutter/material.dart';
+
+// void main()
+// {
+//   runApp(
+//     MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           body: Stack(
+//             children: [
+//               Padding(
+//                 padding: const EdgeInsets.all(10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   color: Colors.blue.shade100,
+//                   child: Icon(Icons.settings),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 70,top: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   color: Colors.blue.shade200,
+//                   child: Icon(Icons.save),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 130,top: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   color: Colors.blue.shade300,
+//                   child: Icon(Icons.download),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 190,top: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   color: Colors.blue.shade400,
+//                   child: Icon(Icons.warning),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 250,top: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   color: Colors.blue.shade500,
+//                   child: Icon(Icons.dangerous),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 310,top: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   color: Colors.blue.shade600,
+//                   child: Icon(Icons.info),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(top: 100,left: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                       color: Colors.blue.shade600,
+//                       borderRadius: BorderRadius.circular(20)
+//                   ),
+//                   child: Icon(Icons.settings),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 70,top: 100),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                       color: Colors.blue.shade500,
+//                       borderRadius: BorderRadius.circular(20)
+//                   ),
+//                   child: Icon(Icons.save),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 130,top: 100),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                       color: Colors.blue.shade400,
+//                       borderRadius: BorderRadius.circular(20)
+//                   ),
+//                   child: Icon(Icons.download),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 190,top: 100),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                       color: Colors.blue.shade300,
+//                       borderRadius: BorderRadius.circular(20)
+//                   ),
+//                   child: Icon(Icons.warning),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 250,top: 100),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                       color: Colors.blue.shade200,
+//                       borderRadius: BorderRadius.circular(20)
+//                   ),
+//                   child: Icon(Icons.dangerous),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 310,top: 100),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                       color: Colors.blue.shade100,
+//                       borderRadius: BorderRadius.circular(20)
+//                   ),
+//                   child: Icon(Icons.info),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(top: 180,left: 10),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.blue.shade100,width: 3),
+//                   ),
+//                   child: Icon(Icons.settings),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 70,top: 180),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.blue.shade200,width: 3),
+//                   ),
+//                   child: Icon(Icons.save),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 130,top: 180),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.blue.shade300,width: 3),
+//                   ),
+//                   child: Icon(Icons.download),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 190,top: 180),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.blue.shade400,width: 3),
+//                   ),
+//                   child: Icon(Icons.warning),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 250,top: 180),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.blue.shade500,width: 3),
+//                   ),
+//                   child: Icon(Icons.dangerous),
+//                 ),
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 310,top: 180),
+//                 child: Container(
+//                   height: 50,
+//                   width: 50,
+//                   decoration: BoxDecoration(
+//                     border: Border.all(color: Colors.blue.shade600,width: 3),
+//                   ),
+//                   child: Icon(Icons.info),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
+-----------------------------------------------------task2--------------------------------------------
+
+
 import 'package:flutter/material.dart';
 
 void main()
 {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
-          body: Stack(
+          body: Column(
             children: [
               Padding(
                 padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  color: Colors.blue.shade100,
-                  child: Icon(Icons.settings),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 70,top: 10),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue.shade200,
-                  child: Icon(Icons.save),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 130,top: 10),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue.shade300,
-                  child: Icon(Icons.download),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 190,top: 10),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue.shade400,
-                  child: Icon(Icons.warning),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 250,top: 10),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue.shade500,
-                  child: Icon(Icons.dangerous),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 310,top: 10),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  color: Colors.blue.shade600,
-                  child: Icon(Icons.info),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 100,left: 10),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue.shade600,
-                      borderRadius: BorderRadius.circular(20)
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("   "),
+                      Icon(Icons.settings,color: Colors.white),
+                      Text(
+                        " Primery",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.settings),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 70,top: 100),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue.shade500,
-                      borderRadius: BorderRadius.circular(20)
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("  "),
+                      Icon(Icons.save,color: Colors.white,size: 20,),
+                      Text(
+                        " Secondary",
+                        style: TextStyle(color: Colors.white,fontSize: 13),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.save),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 130,top: 100),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue.shade400,
-                      borderRadius: BorderRadius.circular(20)
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("  "),
+                      Icon(Icons.download,color: Colors.white),
+                      Text(
+                        " Success",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.download),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 190,top: 100),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue.shade300,
-                      borderRadius: BorderRadius.circular(20)
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("  "),
+                      Icon(Icons.warning,color: Colors.white),
+                      Text(
+                        " warning",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.warning),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 250,top: 100),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue.shade200,
-                      borderRadius: BorderRadius.circular(20)
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("   "),
+                      Icon(Icons.dangerous,color: Colors.white),
+                      Text(
+                        " Danger",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.dangerous),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 310,top: 100),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                      color: Colors.blue.shade100,
-                      borderRadius: BorderRadius.circular(20)
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("      "),
+                      Icon(Icons.info,color: Colors.white),
+                      Text(
+                        " info",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.info),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 180,left: 10),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade100,width: 3),
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("      "),
+                      Icon(Icons.browse_gallery,color: Colors.white),
+                      Text(
+                        " light",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.settings),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.only(left: 70,top: 180),
+                padding: const EdgeInsets.all(10),
                 child: Container(
                   height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade200,width: 3),
+                  width: 100,
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: Row(
+                    children: [
+                      Text("     "),
+                      Icon(Icons.delete,color: Colors.white),
+                      Text(
+                        " Dark",
+                        style: TextStyle(color: Colors.white,fontSize: 15),
+                      ),
+                    ],
                   ),
-                  child: Icon(Icons.save),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 130,top: 180),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade300,width: 3),
-                  ),
-                  child: Icon(Icons.download),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 190,top: 180),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade400,width: 3),
-                  ),
-                  child: Icon(Icons.warning),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 250,top: 180),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade500,width: 3),
-                  ),
-                  child: Icon(Icons.dangerous),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 310,top: 180),
-                child: Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.blue.shade600,width: 3),
-                  ),
-                  child: Icon(Icons.info),
                 ),
               ),
             ],
